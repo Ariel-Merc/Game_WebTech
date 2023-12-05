832123d70c66390b109c42d7f26d645bca1739c8
Ariel-Merc <am4007@g.rit.edu>
2023-12-05 01:27:01 -0500

    not sure whats happening rn, but tomorrows well todays goal is to get player and collisions running

--- project3/js/classes.js
+++ project3/js/classes.js
@@ -8,3 +8,47 @@ class Player extends PIXI.Sprite {
         // this.y = y;
     }
 }
+
+class FirstLayer extends PIXI.Sprite {
+    constructor(x=0, y=0) {
+        // get the background asset
+        super(app.loader.resources["../images/background_0.png"].texture);
+        this.anchor.set(0, 0);
+        this.scale.set(3);
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class SecondLayer extends PIXI.Sprite {
+    constructor(x=0, y=0) {
+        // get the background asset
+        super(app.loader.resources["../images/background_1.png"].texture);
+        this.anchor.set(0, 0);
+        this.scale.set(3);
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class ThirdLayer extends PIXI.Sprite {
+    constructor(x=0, y=0) {
+        // get the background asset
+        super(app.loader.resources["../images/background_2.png"].texture);
+        this.anchor.set(0, 0);
+        this.scale.set(3);
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class SceneLayer extends PIXI.Sprite {
+    constructor(x=0, y=15) {
+        // get the background asset
+        super(app.loader.resources["../images/jumpBG.png"].texture);
+        this.anchor.set(0, 0);
+        this.scale.set(3);
+        this.x = x;
+        this.y = y;
+    }
+}
--- project3/js/main.js
+++ project3/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 const app = new PIXI.Application({
-    width: 900,
-    height: 800
+    width: 864,
+    height: 540
 });
 document.body.appendChild(app.view);
 
@@ -9,17 +9,32 @@ document.body.appendChild(app.view);
 const sceneWidth = app.view.width;
 const sceneHeight = app.view.height;
 
+// pre-load the images (this code works with PIXI v6)
+app.loader.
+    add([
+        "../images/background_0.png",
+        "../images/background_1.png",
+        "../images/background_2.png",
+        "../images/Cat-2/Cat-2-Walk.png",
+        "../images/jumpBG.png"
+    ]);
+app.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
+app.loader.onComplete.add(setup);
+app.loader.load();
+
 // aliases
 let stage;
 
 // game variables
 let startScene;
-let gameScene, star, scoreLabel, lifeLabel, jumpSound, collectSound;
+let gameScene, layerOne, layerTwo, layerThree, layerFour, star, scoreLabel, lifeLabel, jumpSound, collectSound;
 let gameOverScene;
 
 let stars = [];
 let score = 0;
 let gameOverScoreLabel;
+let catTextures;
+let cats = [];
 let lives = 50;
 let levelNum = 1;
 let paused = true;
@@ -29,11 +44,13 @@ function setup() {
     
     // Create start scene
     startScene = new PIXI.Container();
+    startScene.visible = false;
     stage.addChild(startScene);
+    
 
-    // Craete game scene
+    // Create game scene
     gameScene = new PIXI.Container();
-    gameScene.visible = false;
+    gameScene.visible = true;
     stage.addChild(gameScene);
 
     // #3 - Create the `gameOver` scene and make it invisible
@@ -42,11 +59,26 @@ function setup() {
     stage.addChild(gameOverScene);
 
     // #4 - Create labels for all 3 scenes
-    createLabelsAndButtons();
-
+    //createLabelsAndButtons();
+    
+    // create background
+    layerOne = new FirstLayer();
+    gameScene.addChild(layerOne);
+    layerTwo = new SecondLayer();
+    gameScene.addChild(layerTwo);
+    layerThree = new ThirdLayer();
+    gameScene.addChild(layerThree);
+    layerFour = new SceneLayer();
+    gameScene.addChild(layerFour);
+    
     // Create player
-    player = new Player();
-    gameScene.addChild(player);
+    // player = new Player();
+    // gameScene.addChild(player);
+
+    // UGHHHHHHH
+    catTextures = loadSpriteSheet();
+    createCat(0,0,25,25);
+    
 
     // Load Sounds
     // shootSound = new Howl({
@@ -64,4 +96,32 @@ function setup() {
 
     // Now our `startScene` is visible
     // Clicking the button calls startGame()
+}
+
+function loadSpriteSheet(){
+    let spriteSheet = PIXI.BaseTexture.from("../images/Cat-2/Cat-2-Walk.png");
+    let width = 50;
+    let height = 50;
+    console.log(height);
+    let numFrames = 8;
+    let textures = [];
+    for (let i=0; i<numFrames; i++){
+        let frame = new PIXI.Texture(spriteSheet, new PIXI.Rectangle(i*width, 50, width, height));
+        textures.push(frame);
+    }
+    return textures;
+}
+
+function createCat(x, y, frameWidth, frameHeight){
+    let w2 = frameWidth/2;
+    let h2 = frameHeight/2;
+    let catAnim = new PIXI.AnimatedSprite(catTextures);
+    catAnim.x = x- w2;
+    catAnim.y = y - h2;
+    catAnim.animatedSpeed = 1/7;
+    catAnim.loop = true;
+    //catAnim.onComplete = e => gameScene.removeChild(catAnim);
+    cats.push(catAnim);
+    gameScene.addChild(catAnim);
+    catAnim.play();
 }
